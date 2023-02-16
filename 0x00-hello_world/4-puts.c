d00aaaa31ad8607cf41610af55d6b6837ecf4c32
FELIX <felixnyangoka@gmail.com>
2023-02-16 14:10:15 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - prints out line of code using puts
+ * Return : 0
+*/
+int main(void)
+{
+	puts("\" programming is like building a multilingual puzzle");
+	return (0);
+}
