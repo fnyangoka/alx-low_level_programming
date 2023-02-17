640d9dbd64933b803a08fc01c53ae4f47887c935
FELIX <felixnyangoka@gmail.com>
2023-02-16 16:21:31 -0800

    task 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main -Prints a line of code using puts
+ * main -Print a line of code using puts
  *
  * Return : 0
 */
+
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -Prints a code using printf
+ * printf - is the output of the code
+ * Return :0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
