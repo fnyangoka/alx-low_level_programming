f504b675266dd000cf5b3b62e8162e7ac3fc1709
FELIX <felixnyangoka@gmail.com>
2023-02-18 09:00:18 -0800

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,16 +12,22 @@ int main(void)
 	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
+		{
 			int first_digit = x % 10;
 			int second_digit = y % 10;
 
-			if (first_digit != second_digit)
+			if (first_digit != second_digi)
+			{
 				putchar(first_digit + '0');
 				putchar(second_digit + '0');
 
 				if (x != 9 && y != 9)
+				{
 					putchar(',');
 					putchar(' ');
+				}
+			}
+		}
 	}
 
 	putchar('\n');
