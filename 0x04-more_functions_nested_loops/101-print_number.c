cfda16a859dbfe6bea667b91fa9a67ec20c460bb
FELIX <felixnyangoka@gmail.com>
2023-02-27 07:09:47 -0800

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,29 +1,22 @@
 #include "main.h"
 
 /**
- * print_triangle - Prints a triangle, using the character #.
- * @size: The size of the triangle.
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
-void print_triangle(int size)
+void print_number(int n)
 {
-	int hash, index;
+	unsigned int num = n;
 
-	if (size > 0)
+	if (n < 0)
 	{
-		for (hash = 1; hash <= size; hash++)
-		{
-			for (index = size - hash; index > 0; index--)
-				_putchar(' ');
-
-			for (index = 0; index < hash; index++)
-				_putchar('#');
-
-			if (hash == size)
-				continue;
-
-			_putchar('\n');
-		}
+		_putchar('-');
+		num = -num;
 	}
 
-	_putchar('\n');
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
+
