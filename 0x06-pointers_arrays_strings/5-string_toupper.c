8694541edc9c218508d9892d5d31cb3648681d1a
FELIX <felixnyangoka@gmail.com>
2023-03-02 12:03:25 -0800

    task 6

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int y;
+
+	y = 0;
+	while (n[y] != '\0')
+	{
+		if (n[y] >= 'a' && n[y] <= 'z')
+			n[y] = n[y] - 32;
+		y++;
+	}
+	return (n);
+}
+
